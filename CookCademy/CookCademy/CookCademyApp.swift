d44092a2c0fa86b8c00223258fab0208e6ea72a0
Baybar1 <branleyjraybar@gmail.com>
2022-12-30 19:18:16 -0500

    used the list structure to build a list of zoo animals

---
+++ CookCademy/CookCademy/CookCademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookCademyApp.swift
+//  CookCademy
+//
+//  Created by user232116 on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CookCademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZooKeeper/ZooKeeper/ZooKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZooKeeperApp.swift
+//  ZooKeeper
+//
+//  Created by user232116 on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct ZooKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
