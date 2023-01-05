2084132ad615f84fb8c24e8a3c2c63f9eec7531c
Baybar1 <branleyjraybar@gmail.com>
2023-01-04 21:34:32 -0500

    made a calendar using the LazyVGrid and GridItem implemented in Swift

---
+++ GridCalendar/GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by user232116 on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
